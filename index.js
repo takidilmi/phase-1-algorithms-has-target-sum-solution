84e547e0b91dfa255cbcd96bd25fe989c750cede
takidilmi <angeldilmi@gmail.com>
2023-09-01 20:29:53 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const visitedNumbers = new Set();
+
+  for (const num of array) {
+    const difference = target - num;
+    
+    if (visitedNumbers.has(difference)) {
+      return true;
+    }
+    
+    visitedNumbers.add(num);
+  }
+  
+  return false;
 }
 
 /* 
-  Write the Big O time complexity of your function here
+  O(n)
+  
 */
 
 /* 
-  Add your pseudocode here
+  1. Create an empty set called visitedNumbers to keep track of visited numbers.
+  2. Iterate through each number, num, in the input array:
+   a. Calculate the difference as target - num.
+   b. Check if the difference is in the visitedNumbers set:
+      - If yes, return true because there exists a pair of numbers that adds up to the target.
+   c. Add num to the visitedNumbers set to mark it as visited.
+  3. If the loop completes without finding a pair, return false because no pair adds up to the target.
+
 */
 
 /*
